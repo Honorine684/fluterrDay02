8b51198fdd49f16fd0d7f74363031ad069c43e49
Honorine684 <honorinegabiam0@gmail.com>
2025-02-17 10:40:29 +0100

    mise en place de mon botombar et gestion de la navigation entre les pages

---
+++ pay/lib/Home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget{
+  const Home({super.key});
+
+  @override
+  State<Home> createState() {
+    return HomeState();
+  }
+
+}
+class HomeState extends State<Home>{
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+    );
+  }
+  
+}
